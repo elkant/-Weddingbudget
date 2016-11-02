b9e6b65db2d47cb394cca58af0ebf4e013ac765e
Emmanuel Kaunda <elkant@users.noreply.github.com>
2016-11-02 08:04:46 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,3 +1,4 @@
+
 <?php
 
 function verify($sendermail, $username, $verficationcode, $accountype) {
@@ -10,7 +11,7 @@ function verify($sendermail, $username, $verficationcode, $accountype) {
     $mail->Host = 'weddingbudget.co.ke';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'noreply@weddingbudget.co.ke';                 // SMTP username
-    $mail->Password = '';                           // SMTP password
+    $mail->Password = 'P@ss4wb!';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 25;                                    // TCP port to connect to
     $mail->setFrom('noreply@weddingbudget.co.ke', 'Wedding Budget');
@@ -44,4 +45,47 @@ function verify($sendermail, $username, $verficationcode, $accountype) {
         $resp = 'Message has been sent';
     }
     return $resp;
+}
+
+
+
+function anymail($sendermail,$sendername, $dyn_subject,$dyn_body) {
+
+    require 'vendor/autoload.php';
+
+    $mail = new PHPMailer;
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'weddingbudget.co.ke';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = $sendermail;                 // SMTP username
+    $mail->Password = 'P@ss4wb!';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 25;                                    // TCP port to connect to
+    $mail->setFrom($sendermail, $sendername);
+
+
+    $mail->addAddress($sendermail);     // Add a recipient
+//$mail->addAddress('elkaunda@gmail.com');               // Name is optional
+    $mail->addReplyTo($sendermail, $sendername);
+    $mail->addBCC('elkaunda@gmail.com');
+    $mail->addBCC('mollymumbi@gmail.com');
+    $mail->AddEmbeddedImage('img/logo.png', 'logo_2u');
+    // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->Subject = $dyn_subject;
+    $mail->Body = $dyn_body;
+    $mail->AltBody = '';
+    $resp = "";
+    if (!$mail->send()) {
+
+        $resp = 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+
+        $resp = 'Message has been sent';
+    }
+    return $resp;  
+    
 }
